d9e7c8460d4afc17c7e13bd726dd8c59d6f71ce7
Samuel Borick <sb205@254-10.cs.uakron.edu>
2016-11-02 14:19:47 -0400

    Initial Commit

---
+++ iOSMidterm/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  iOSMidterm
+//
+//  Created by Borick,Samuel on 11/2/16.
+//  Copyright © 2016 Borick,Samuel. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
